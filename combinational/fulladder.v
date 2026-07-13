a7f160af77a52ee8666dc129b0f0495ff302be7f
khavyakj <160817279+khavyakj@users.noreply.github.com>
2026-07-13 18:51:48 +0530

    fulladder.v

---
+++ combinational/fulladder.v
@@ -0,0 +1,15 @@
+// Code your design here
+module fulladder(
+	input wire a,
+  	input wire b,
+  	input wire cin,
+  	output wire sum,
+  	output wire cout
+
+
+);
+  
+  
+  assign sum = (a^b^cin);
+  assign cout= ((a&b)|(b&cin)|(a&cin));
+endmodule
